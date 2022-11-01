03c4e329b384695ecbc8a573fa87c11a5755fb18
Noel <14931745+FireRedNinja@users.noreply.github.com>
2022-11-01 19:43:04 +0000

    Lazy Loaded Images (#2)
    * added lazy loaded images
    
    * format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })],
   site: "https://fireredninja.github.io/",
-  base: "/art-of-runeterra",
+  base: "/art-of-runeterra"
 });
